37fc2a0df9b9cedaf2cd500e94d83087951a1ee4
Ben Sparrow <bensparrowdev@gmail.com>
2021-03-30 16:48:34 +0100

    connected to db

---
+++ src/db/connection.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true 
+        });
+        console.log("successfully connected to db")
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
