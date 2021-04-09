297a4bdfc07f828798969f92d3cbc3a5c1b087f9
Ben Sparrow <bensparrowdev@gmail.com>
2021-04-09 11:48:37 +0100

    added blog posting functionality for users

--- src/server.js
+++ src/server.js
@@ -5,107 +5,108 @@ const { Post } = require("./models/Post");
 
 const port = process.env.PORT || 5000;
 // init instance of express
-const app = express()
+const app = express();
 
 //Middleware
 app.use(express.json());
 
 //routes/endpoints health of server
 app.get("/health", (req, res) => {
-    console.log(req.body);
-    res.send({
-        message: "the server is working"
-    });
+  console.log(req.body);
+  res.send({
+    message: "the server is working",
+  });
 });
 
-//User
+//User accounts
 app.get("/user", async (req, res) => {
-    //this is the route to get a user
-    try {
-        const allUsers = await User.find({});
-        res.status(200).send(allUsers);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
+  //this is the route to get a user
+  try {
+    const allUsers = await User.find({});
+    res.status(200).send(allUsers);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 app.post("/user", async (req, res) => {
-    //this is the route to add a user
-    try {
-        const user = new User(req.body);
-        const returnValue = await user.save();
-        console.log(`Successfully added ${returnValue.name}`)
-        res.status(201).send(`Successfully added ${returnValue.name}`);
-    } catch (error) {
-        res.status(400).send(error);
-    }
+  //this is the route to add a user
+  try {
+    const user = new User(req.body);
+    const returnValue = await user.save();
+    console.log(`Successfully added ${returnValue.name}`);
+    res.status(201).send(`Successfully added ${returnValue.name}`);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 app.patch("/user/:id", async (req, res) => {
-    //this will be our route to update a user
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        console.log(user);
-        res.status(200).send(user);
-    } catch (error) {
-        res.status(404).send({
-            message: "user not found"
-        })
-    }
-})
+  //this will be our route to update a user
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    console.log(user);
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(404).send({
+      message: "user not found",
+    });
+  }
+});
 
 app.delete("/user/:id", async (req, res) => {
-    //this will be our route to delete a user
-    try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        res.status(200).send(user);
-    } catch (error) {
-        res.status(400).send({
-            message: "user not found"
-        });
-    }
+  //this will be our route to delete a user
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send({
+      message: "user not found",
+    });
+  }
 });
 
-
-
-
-//Blogpost
-app.get("/post", async (req, res) => {
-    try {
-        const allPosts = await Post.find({})
-        res.status(200).send(allPosts)
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(error)
-    }
+//Blogposts
+app.get("/posts", async (req, res) => {
+  //gets all posts
+  try {
+    const allPosts = await Post.find({});
+    res.status(200).send(allPosts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
-app.get("/post/:user_id", async (req, res) => {
-    try {
-        const allPosts = await Post.find({
-            author: req.params.user_id
-        })
-        res.status(200).send(allPosts)
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(error)
-    }
+app.get("/posts/:user_id", async (req, res) => {
+  //get all posts by user_id
+  try {
+    const allPosts = await Post.find({ author: req.params.user_id });
+    res.status(200).send(allPosts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
-app.post("/post/:user_id", async (req, res) => {
-    try {
-        const post = new Post(req.body);
-        post.author = req.params.user_id;
-        const returnedValue = await post.save();
+app.post("/posts/:user_id", async (req, res) => {
+  //creates a post by user_id
+  try {
+    const post = new Post(req.body);
+    post.author = req.params.user_id;
+    const returnedValue = await post.save();
 
-        res.status(201).send(returnedValue);
-    } catch (error) {
-        res.status(400).send(error);
-    }
+    res.status(201).send(returnedValue);
+    console.log("New post created");
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
 });
-
 
 //server
 app.listen(5000, () => {
-    console.log(`Server listening on port ${port}`);
-})
+  console.log(`Server listening on port ${port}`);
+});
